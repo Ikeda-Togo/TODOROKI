91521232ca3c03245e39ac5c16fd54e0df2e0919
roboroborx782@gmail.com <roboroborx782@gmail.com>
2020-02-06 10:48:39 +0000

    success

---
+++ control_motor/PC_control/pc_input.py
@@ -0,0 +1,56 @@
+import time
+import sys
+import termios
+import os
+import serial
+
+sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+import blv_lib
+client = serial.Serial("/dev/ttyXRUSB0",115200,timeout=0.1,parity=serial.PARITY_EVEN,stopbits=serial.STOPBITS_ONE)
+
+motor1=blv_lib.blv_motor(client,1)
+motor2=blv_lib.blv_motor(client,2)
+
+while True:
+    fd = sys.stdin.fileno()
+
+    old = termios.tcgetattr(fd)
+    new = termios.tcgetattr(fd)
+
+    new[3] &= ~termios.ICANON
+
+    new[3] &= ~termios.ECHO
+
+    try:
+        termios.tcsetattr(fd,termios.TCSANOW,new)
+
+        ch = sys.stdin.read(1)
+
+    finally:
+        termios.tcsetattr(fd,termios.TCSANOW,old)
+
+    print(ch)
+    #print(ch.encode('utf-8'))
+    if ch=='w':
+        print("Advance foward")
+        motor1.set_speed_and_go(100,1,0)
+        motor2.set_speed_and_go(100,0,1)
+    elif ch=='s':
+        print("Back")
+        motor1.set_speed_and_go(100,0,1)
+        motor2.set_speed_and_go(100,1,0)
+    elif ch=='a':
+        print("Left")
+        motor1.set_speed_and_go(100,0,1)
+        motor2.set_speed_and_go(100,0,1)
+    elif ch=='d':
+        print("Right")
+        motor1.set_speed_and_go(100,1,0)
+        motor2.set_speed_and_go(100,1,0)
+    elif ch== ' ':
+        print("Stop")
+        motor1.set_speed_and_go(0,0,0)
+        motor2.set_speed_and_go(0,0,0)
+    elif ch=="q":
+        break
+
---
+++ control_motor/PC_control/remotecenter.py
@@ -0,0 +1,49 @@
+import serial
+import time
+import sys
+import termios
+import os
+import serial
+
+sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+import blv_lib
+import az_lib_direct
+client = serial.Serial("/dev/ttyXRUSB0",115200,timeout=0.1,parity=serial.PARITY_EVEN,stopbits=serial.STOPBITS_ONE)
+
+RC_mode=1
+
+motor5=az_lib_direct.az_motor_direct(client,5,[0,58436,90000,116750])
+
+motor5.go_list(RC_mode)
+
+
+while True:
+    fd = sys.stdin.fileno()
+
+    old = termios.tcgetattr(fd)
+    new = termios.tcgetattr(fd)
+
+    new[3] &= ~termios.ICANON
+
+    new[3] &= ~termios.ECHO
+
+    try:
+        termios.tcsetattr(fd,termios.TCSANOW,new)
+
+        ch = sys.stdin.read(1)
+
+    finally:
+        termios.tcsetattr(fd,termios.TCSANOW,old)
+
+    print(ch)
+    #print(ch.encode('utf-8'))
+    if ch=='w'and RC_mode<3:
+        print("Advance foward")
+        RC_mode+=1
+        motor5.go_list(RC_mode)
+    elif ch=='s'and RC_mode>0:
+        print("Back")
+        RC_mode-=1
+        motor5.go_list(RC_mode)
+    elif ch=="q":
+        break
