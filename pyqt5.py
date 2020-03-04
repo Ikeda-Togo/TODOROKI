fbfb554e05ea0e1117ae53f12a3aea2e8b228ca4
IkedaTogo <47003257+Ikeda-Togo@users.noreply.github.com>
2020-03-04 19:48:46 +0900

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -232,12 +232,14 @@ def Play():
     print("hello")
     app.exec_()
 def Play2():
-    print("hello world")
-    App.getdata
+    app2 = QApplication(sys.argv)
+    ex2 = App()
+    print("hello")
+    app2.exec_()
 
     
 if __name__ == "__main__":
     th1 =threading.Thread(target=Play)
-    th2 =threading.Thread(target=Play)
+    th2 =threading.Thread(target=Play2)
     th1.start()
     th2.start()
