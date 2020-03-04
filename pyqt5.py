2c6f1b579e02a3c27ba97d5b1788e9266fa9adb9
IkedaTogo <47003257+Ikeda-Togo@users.noreply.github.com>
2020-03-04 19:45:52 +0900

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -238,6 +238,6 @@ def Play2():
     
 if __name__ == "__main__":
     th1 =threading.Thread(target=Play)
-    th2 =threading.Thread(target=Play2)
+    th2 =threading.Thread(target=Play)
     th1.start()
     th2.start()
