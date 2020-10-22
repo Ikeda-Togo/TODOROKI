d719d12202d2ff17022a009e3105cbcd83950051
tamago <youhoge@example.com>
2020-10-22 14:38:13 +0900

    mkdir todoroki_arm

--- control_robotarm/todoroki_arm/IKPy_test.py
+++ control_robotarm/todoroki_arm/IKPy_test.py
@@ -1,6 +1,7 @@
 import ikpy
 import numpy as np
 import time
+import ikpy.utils.plot as plot_utils
 # import ikpy.utils.plot as plot_utils
 
 # my_chain = ikpy.chain.Chain.from_urdf_file("urdf/model.URDF")
@@ -16,5 +17,7 @@ ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
 
 my_chain.plot(my_chain.inverse_kinematics([0.8, 0.2, 0.2]), ax)
 my_chain.plot(my_chain.inverse_kinematics([0.7, 0.2, 0.2]), ax)
+my_chain.plot(my_chain.inverse_kinematics([0.6, 0.2, 0.2]), ax)
+# my_chain.plot(my_chain.inverse_kinematics([0.7, 0.2, 0.2]), ax)
 
 matplotlib.pyplot.show()
