7e0c66cb0ad17af18c60dbc622c4d494e93beacc
tamago <youhoge@example.com>
2020-10-25 09:38:27 +0900

    update

---
+++ control_robotarm/IKPy_test.py
@@ -0,0 +1,23 @@
+import ikpy
+import numpy as np
+import time
+import ikpy.utils.plot as plot_utils
+# import ikpy.utils.plot as plot_utils
+
+# my_chain = ikpy.chain.Chain.from_urdf_file("urdf/model.URDF")
+my_chain = ikpy.chain.Chain.from_urdf_file("todoroki_robotarm.URDF")
+
+# my_chain.forward_kinematics([0] * 8)
+# my_chain.inverse_kinematics([2, 2, 2])
+
+
+import matplotlib.pyplot
+from mpl_toolkits.mplot3d import Axes3D
+ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
+
+my_chain.plot(my_chain.inverse_kinematics([0.8, 0.4, 0.4]), ax)
+my_chain.plot(my_chain.inverse_kinematics([0.7, 0.2, 0.2]), ax)
+my_chain.plot(my_chain.inverse_kinematics([0.6, 0.2, 0.2]), ax)
+# my_chain.plot(my_chain.inverse_kinematics([0.7, 0.2, 0.2]), ax)
+
+matplotlib.pyplot.show()
--- control_robotarm/todoroki_arm/IKPy_test.py
+++ control_robotarm/todoroki_arm/IKPy_test.py
@@ -2,22 +2,31 @@ import ikpy
 import numpy as np
 import time
 import ikpy.utils.plot as plot_utils
-# import ikpy.utils.plot as plot_utils
-
-# my_chain = ikpy.chain.Chain.from_urdf_file("urdf/model.URDF")
-my_chain = ikpy.chain.Chain.from_urdf_file("todoroki_robotarm.URDF")
-
-# my_chain.forward_kinematics([0] * 8)
-# my_chain.inverse_kinematics([2, 2, 2])
-
-
 import matplotlib.pyplot
 from mpl_toolkits.mplot3d import Axes3D
 ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
 
-my_chain.plot(my_chain.inverse_kinematics([0.8, 0.2, 0.2]), ax)
-my_chain.plot(my_chain.inverse_kinematics([0.7, 0.2, 0.2]), ax)
-my_chain.plot(my_chain.inverse_kinematics([0.6, 0.2, 0.2]), ax)
+# my_chain = ikpy.chain.Chain.from_urdf_file("urdf/model.URDF")
+my_chain = ikpy.chain.Chain.from_urdf_file("todoroki_robotarm.URDF")
+
+home_pos = my_chain.forward_kinematics([0] * 8)[:,3][0:3]
+print("forward_kinematics", home_pos)
+# my_chain.inverse_kinematics([2, 2, 2])
+my_chain.plot(my_chain.inverse_kinematics(home_pos), ax)
+
+
+# for i in range(-4,5):
+#     x = i * 0.1
+#     my_chain.plot(my_chain.inverse_kinematics([x, 0.5, 0.5]), ax)
+
+# for i in range(-4,5):
+#     y = i * 0.1
+#     my_chain.plot(my_chain.inverse_kinematics([0, y, 0.5]), ax)
+
+# for i in range(0,10):
+#     z = i * 0.1
+#     my_chain.plot(my_chain.inverse_kinematics([0, 0.5, z]), ax)
 # my_chain.plot(my_chain.inverse_kinematics([0.7, 0.2, 0.2]), ax)
+print(my_chain.inverse_kinematics(home_pos))
 
 matplotlib.pyplot.show()
