c111c0db0dcbf289507a554c4eff917623ed52a2
tamago <youhoge@example.com>
2020-10-26 02:40:09 +0900

    update

--- control_robotarm/todoroki_arm/qt_B3Mcontroller.py
+++ control_robotarm/todoroki_arm/qt_B3Mcontroller.py
@@ -4,7 +4,7 @@
 import b3mCtrl
 import time
 import sys
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QLineEdit, QHBoxLayout, QVBoxLayout, QGridLayout, QLabel
+from PyQt5.QtWidgets import (QWidget, QLabel, QPushButton, QComboBox, QMainWindow, QApplication)
 
 
 class Example(QMainWindow):
@@ -13,6 +13,7 @@ class Example(QMainWindow):
         super().__init__()
 
         self.initUI()
+
         self.pos = [0] * 10
 
         self.aaa = b3mCtrl.B3mClass()
@@ -20,60 +21,55 @@ class Example(QMainWindow):
         print (self.aaa.setTrajectoryType(255,"EVEN"))
         print (self.aaa.setMode(255,"POSITION"))
         # self.showMaximized() 
-    
+
     def initUI(self):      
 
         btn1_add = QPushButton("+", self)
         btn1_add.move(140, 20)
-    
-    
+        
         btn1_sub = QPushButton("-", self)
         btn1_sub.move(140, 80)
 
-        self.lbl = QLabel(self)
-        self.lbl.move(30, 60)
-
-        self.inputText = QLineEdit(self)
-        self.inputText.move(30, 20)
-        self.inputText.textChanged[str].connect(self.onChanged)
         free_btn =QPushButton("FREE_MODE", self)
-        free_btn.move(30, 80)
+        free_btn.move(140, 130)
 
-
-        # クリックされたらbuttonClickedの呼び出し
         btn1_add.clicked.connect(self.buttonClicked)            
         btn1_sub.clicked.connect(self.buttonClicked)            
-        free_btn.clicked.connect(self.buttonClicked)            
-        # btn2.clicked.connect(self.buttonClicked)
+        free_btn.clicked.connect(self.buttonClicked) 
 
+        # ラベル作成、初期の名前をUbuntuにする
+        self.lbl = QLabel("Servo id", self)
+
+        # QComboBoxオブジェクトの作成
+        combo = QComboBox(self)
+        # アイテムの名前設定
+        for id in range(0,10):
+            combo.addItem(str(id))
+
+        combo.move(30, 20)
+        self.lbl.move(30, 80)
+        
         self.statusBar()
 
-        self.setGeometry(300, 400, 290, 150)
-        self.setWindowTitle('Event sender')
+        # アイテムが選択されたらonActivated関数の呼び出し
+        combo.activated[str].connect(self.onActivated)        
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('QComboBox')
         self.show()
 
-    def output(self):
-        self.statusBar().showMessage(self.inputText.text())
 
-    def onChanged(self, text):
-        print(str(text))
-        if text
-
+    def onActivated(self, text):
         self.id = int(text)
-        self.pos[self.id]=0
-        # ラベルに入力されたテキストを挿入
+        # ラベルに選択されたアイテムの名前を設定
         self.lbl.setText(text)
-        # 入力されたテキストによって、ラベルの長さを調整
-        self.lbl.adjustSize() 
+        # ラベルの長さを調整
+        self.lbl.adjustSize()  
 
+   
     def buttonClicked(self):
-
         # ステータスバーへメッセージの表示
         sender = self.sender()
-
-        # if sender.text() != "+" and sender.text() != "-":
-        #     self.now_id = sender.text()
-        #     time.sleep(0.01)
 
         if sender.text() == "+":
             self.pos[self.id] += 1000 
@@ -93,9 +89,7 @@ class Example(QMainWindow):
             pass
         self.statusBar().showMessage('id '+ str(self.id) + ' position is ' + str(self.pos[self.id]))
 
-
 if __name__ == '__main__':
-
 
     app = QApplication(sys.argv)
     ex = Example()
