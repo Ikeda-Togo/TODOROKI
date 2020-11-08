591b33202fc8a2aa11c9f9c6dd29e287559a7c58
roboroborx782@gmail.com <roboroborx782@gmail.com>
2020-11-08 13:59:03 +0900

    update

--- control_motor/PC_control/blv_test1.py
+++ control_motor/PC_control/blv_test1.py
@@ -4,14 +4,22 @@ import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
-import blv_lib
+import blv_test_lib
 client = serial.Serial("/dev/ttyXRUSB0", 115200, timeout=0.1, parity=serial.PARITY_EVEN,stopbits=serial.STOPBITS_ONE) # COMポートは自分の奴を入れる
 #client = serial.Serial("/dev/ttyACM0", 115200, timeout=10, parity=serial.PARITY_EVEN,stopbits=serial.STOPBITS_ONE) # COMポートは自分の奴を入れる
 
-motor1 = blv_lib.blv_motor(client,1)
+motor1 = blv_test_lib.blv_motor(client,2)
 #motor1.set_speed_and_go(100,1,0)
-motor1.set_speed_and_go(1000,0,1)
+# motor1.set_speed_and_go(200,0,1)
 
+motor1.set_speed(int(100))
+motor1.go(0,1)
+print("go:1000")
+time.sleep(5)
+
+motor1.set_speed(1000)
+motor1.go(0,1)
+print("go:100")
 time.sleep(5)
 #time.sleep(3)
 
