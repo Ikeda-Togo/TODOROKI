68c526d8b48685d591d6e0a0ed5902114757bfa7
roboroborx782@gmail.com <roboroborx782@gmail.com>
2021-01-12 18:23:34 +0900

    update

---
+++ test_code/controller.py
@@ -0,0 +1,188 @@
+import usb.core
+import usb.util
+import sys
+from time import gmtime, strftime
+import time
+import threading
+import copy
+import lcm
+from exlcm import example_t
+
+DED_ZONE = 120
+Z_DED_ZONE = 250
+DIFF_SIZE = 5
+Z_DIFF_SIZE = 10
+# dev = usb.core.find(idVendor=0x256f, idProduct=0xc635)
+dev = usb.core.find(idVendor=0x46d, idProduct=0xc626)
+if dev is None:
+    raise ValueError('SpaceNavigator not found');
+else:
+    print('SpaceNavigator found')
+    print(dev)
+
+cfg = dev.get_active_configuration()
+print('cfg is ', cfg)
+intf = cfg[(0,0)]
+print('intf is ', intf)
+ep = usb.util.find_descriptor(intf, custom_match = lambda e: usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_IN)
+print('ep is ', ep)
+
+reattach = False
+if dev.is_kernel_driver_active(0):
+    reattach = True
+    dev.detach_kernel_driver(0)
+
+ep_in = dev[0][(0,0)][0]
+ep_out = dev[0][(0,0)][1]
+
+print('')
+print('Exit by pressing any button on the SpaceNavigator')
+print('')
+
+Z_push = 0
+old_Z_push = 0
+
+#######X,Y,Zの値###########
+R_list = [0,0,0]
+old_R_list = 0
+
+Button_number = 0
+
+Mode = 0
+
+#####################################  LCM      #########################################################
+
+######publishされたら動く############
+def my_handler(channel, data):
+    global msg
+    msg = example_t.decode(data)
+    
+    print("Received message on channel \"%s\"" % channel)
+    print("   mode   = %s" % str(msg.mode))
+    print("   LU_mode   = %s" % str(msg.LU_mode))
+    print("   R_list    = %s" % type(msg.R_list), msg.R_list)
+    print("   Z_push    = %s" % str(msg.Z_push))
+    print("")
+
+
+def subscribe_handler(handle):
+    while True:
+        handle()
+
+
+pub_msg = example_t()
+lc = lcm.LCM()
+subscription = lc.subscribe("EXAMPLE", my_handler)
+
+########handleをwhileでぶん回すのをサブスレッドで行う############
+thread1 = threading.Thread(target=subscribe_handler, args=(lc.handle,))
+thread1.setDaemon(True)
+thread1.start()
+print("入力待ち")
+
+####################################=========================================####################################
+
+run = True
+while run:
+    try:
+        data = dev.read(ep_in.bEndpointAddress, ep_in.bLength, 0)
+
+        #Z軸のプッシュ判定
+        if data[0] == 1:
+            pub_msg.R_list[0],pub_msg.R_list[1],pub_msg.R_list[2]= 0,0,0
+
+            old_Z_push = copy.deepcopy(Z_push)
+            Z_push = data[5] + (data[6]*256)
+
+            if data[6] > 127:
+                Z_push -= 65536
+
+            #デッドゾーンの処理
+            if Z_push <= Z_DED_ZONE and Z_push >= -Z_DED_ZONE:
+                Z_push = 0
+
+            #感度の処理
+            diff = abs(Z_push - old_Z_push)
+            if diff > Z_DIFF_SIZE and sum(R_list) == 0:
+                pub_msg.Z_push=Z_push
+                print("Pub ZPush:",pub_msg.Z_push)
+                lc.publish("EXAMPLE", pub_msg.encode())
+                print("Push: ",Z_push)
+
+        #回転の移動判定
+        elif data[0] == 2:
+
+            pub_msg.Z_push=0
+            old_R_list = copy.deepcopy(R_list)
+            R_list[0] = data[1] + (data[2]*256)
+            R_list[1] = data[3] + (data[4]*256)
+            R_list[2] = data[5] + (data[6]*256)
+            
+            if data[2] > 127:
+                R_list[0] -= 65536
+            if data[4] > 127:
+                R_list[1] -= 65536
+            if data[6] > 127:
+                R_list[2] -= 65536
+
+            #デッドゾーンの処理
+            for i in range(3):
+                if R_list[i] <= DED_ZONE and R_list[i] >= -DED_ZONE :
+                    R_list[i] = 0
+
+            #感度の処理
+            diff = abs(sum(R_list) - sum(old_R_list))
+            if diff > DIFF_SIZE and abs(Z_push) < Z_DED_ZONE:
+                print("R: ", R_list[0], R_list[1], R_list[2])
+                pub_msg.R_list[0],pub_msg.R_list[1],pub_msg.R_list[2]= R_list[0], R_list[1], R_list[2]
+                print("Pub R:",pub_msg.R_list)
+                lc.publish("EXAMPLE", pub_msg.encode())
+
+        #ボタンの判定(左が2,右が1,同時押しが3)
+        if data[0] == 3:
+            if data[1]== 0:
+                # msg.mode = Mode
+                print("push button : ", Button_number)
+                if Button_number == 1:
+                    if msg.mode == 2:
+                        pub_msg.mode = 0
+                    else:
+                        pub_msg.mode += 1
+                    if msg.mode == 1:
+                        RC_flag = 0
+                elif Button_number == 2:
+                    if msg.mode == 0:
+                        pub_msg.mode = 2
+                    else:
+                        pub_msg.mode -= 1
+                    if msg.mode == 1:
+                        RC_flag = 0
+                elif Button_number == 3:
+                    pass
+                print("Now Mode:",pub_msg.mode)
+                lc.publish("EXAMPLE", pub_msg.encode())
+               # Mode = msg.mode
+
+                Button_number = 0
+
+            else:
+                Button_number = data[1]
+                # print("   mode   = %s" % str(Mode))
+
+    except KeyboardInterrupt:
+        print("end")
+        break
+
+    except usb.core.USBError:
+        print("USB error")
+        pub_msg.R_list[0],pub_msg.R_list[1],pub_msg.R_list[2]= 0, 0, 0
+        pub_msg.Z_push=0
+        lc.publish("EXAMPLE", pub_msg.encode())
+        break
+    
+
+# end while
+usb.util.dispose_resources(dev)
+
+if reattach:
+    dev.attach_kernel_driver(0)
---
+++ test_code/oriental.py
@@ -0,0 +1,128 @@
+import usb.core
+import usb.util
+import os 
+import sys
+import serial
+import time
+
+import threading
+import lcm
+from exlcm import example_t
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../control_motor'))
+import blv_lib
+import az_lib_direct
+
+LU_mode = 1 #0:収納, 1:テンション維持モード 2:リフトアップ
+RC_mode = 1 #0:階段降り, 1:真ん中, 2:椅子座り, 3:階段上り
+#RC変数#################################################
+RC_flag = 1         #クリックの判定(1の時は次への移動をしない)
+########################################################
+stop_flag=1
+# id ="/dev/ttyACM1"
+id ="/dev/ttyXRUSB0"
+# id ="/dev/ttyUSB0"
+
+#####################################  LCM      #########################################################
+
+######publishされたら動く############
+def my_handler(channel, data):
+    global msg
+    msg = example_t.decode(data)
+    
+    # print("Received message on channel \"%s\"" % channel)
+    # print("   mode   = %s" % str(msg.mode))
+    # print("   R_list    = %s" % str(msg.R_list))
+    # print("   Z_push    = %s" % str(msg.Z_push))
+    # print("")
+
+    # if msg.mode == 1:
+    #     print("mode:",msg.mode)
+    #     #liftup_remotecenter(msg.R_list,msg.Z_push)
+    #     time.sleep(1)
+
+def subscribe_handler(handle):
+    while True:
+        handle()
+
+msg = example_t()
+lc = lcm.LCM()
+subscription = lc.subscribe("EXAMPLE", my_handler)
+
+########handleをwhileでぶん回すのをサブスレッドで行う############
+thread1 = threading.Thread(target=subscribe_handler, args=(lc.handle,))
+thread1.setDaemon(True)
+thread1.start()
+
+###########################################################################################################
+
+
+client = serial.Serial(id, 115200, timeout=0.1, parity=serial.PARITY_EVEN,stopbits=serial.STOPBITS_ONE) # COMポートは自分の奴を入れる
+client.flushInput()
+client.flushOutput()
+# client = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.1, parity=serial.PARITY_EVEN,stopbits=serial.STOPBITS_ONE) # COMポートは自分の奴を入れる
+#モータのインスタンス化##############################
+motor1 = blv_lib.blv_motor(client,1) #右クローラ
+motor2 = blv_lib.blv_motor(client,2) #左のクローラ
+#####################################################
+
+print("start Crawler.py")
+
+while True :
+    #Mode:0 クローラモード
+    if msg.mode == 0:
+        print(int(abs(80*msg.R_list[0]*0.01)) + int(msg.R_list[2]*0.04))
+        if msg.R_list[0] == 0 and msg.R_list[1] == 0 and msg.R_list[2]==0: #停止
+            # print("Stop")
+            #motor1.set_speed(0)
+            #motor2.set_speed(0)
+            motor1.go(1,1)
+            motor2.go(1,1)
+        elif msg.R_list[0] > 0: #前進移動
+            # print("Advance forward")
+            if msg.R_list[1] >= 0:#左をはやく
+                motor1.set_speed(int(abs(80*msg.R_list[0]*0.01)))
+                motor2.set_speed(int(abs(80*msg.R_list[0]*0.01)) + int(msg.R_list[1]*0.04))
+            elif msg.R_list[1] < 0:#右をはやく
+                motor1.set_speed(int(abs(80*msg.R_list[0]*0.01)) + int(msg.R_list[1]*0.04))
+                motor2.set_speed(int(abs(80*msg.R_list[0]*0.01)))
+            #motor1.go(1,0)
+            #motor2.go(0,1)
+            motor1.go(0,1)
+            motor2.go(1,0)
+        elif msg.R_list[0] < 0:  #後進移動
+            # print("fall back")
+            if msg.R_list[1] >= 0:#左をはやく
+                motor1.set_speed(int(abs(80*msg.R_list[0]*0.01)))
+                motor2.set_speed(int(abs(80*msg.R_list[0]*0.01)) + int(abs(msg.R_list[1]*0.04)))
+            elif msg.R_list[1] < 0:#右をはやく
+                motor1.set_speed(int(abs(80*msg.R_list[0]*0.01)) + int(abs(msg.R_list[1]*0.04)))
+                motor2.set_speed(int(abs(80*msg.R_list[0]*0.01))) 
+            #motor1.go(0,1)
+            #motor2.go(1,0)
+            motor1.go(1,0)
+            motor2.go(0,1)
+        elif msg.R_list[2] > 0: #左は前,右は後ろ
+            # print("R Roll")
+            motor1.set_speed(int(abs(80*msg.R_list[2]*0.01)))
+            motor2.set_speed(int(abs(80*msg.R_list[2]*0.01)))
+            motor1.go(1,0)
+            motor2.go(1,0)
+            
+        elif msg.R_list[2] < 0: #右は前,左は後ろ
+            # print("L Roll")
+            motor1.set_speed(int(abs(80*msg.R_list[2]*0.01)))
+            motor2.set_speed(int(abs(80*msg.R_list[2]*0.01)))
+            motor1.go(0,1)
+            motor2.go(0,1)
+
+        stop_flag=0
+
+    else :
+        if stop_flag == 0:
+            # print("Stop")
+            motor1.go(1,1)
+            motor2.go(1,1)
+            stop_flag=1
+        else:
+            pass
